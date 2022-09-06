eda931b3a8b7b986907af1b20ea8a8abcc30074a
Ilham Yusuf <ilham.0992@students.amikom.ac.id>
2022-09-06 11:45:44 +0700

    [ADD] Navbar & Modal Filter

--- src/component/organism/Navbar.js
+++ src/component/organism/Navbar.js
@@ -42,17 +42,95 @@ function Navbar() {
 				<div className="modal-dialog">
 					<div className="modal-content">
 						<div className="modal-header">
+							<button type="button" className={style.btnModalClose} data-bs-dismiss="modal" aria-label="Close" />
 							<h5 className="modal-title" id="exampleModalLabel">
-								Modal title
+								Filter
 							</h5>
-							<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 						</div>
-						<div className="modal-body">...</div>
+						<form>
+							<div className="modal-body">
+								<h6>Colors</h6>
+							</div>
+							<div className="modal-body">
+								<h6>Sizes</h6>
+								<div className={`d-flex flex-wrap ${style.checkContainer} mt-3`}>
+									<div>
+										<input type="checkbox" class="btn-check" id="xs" autocomplete="off" />
+										<label class="btn btn-outline-primary" for="xs">
+											XS
+										</label>
+									</div>
+									<div>
+										<input type="checkbox" class="btn-check" id="s" autocomplete="off" />
+										<label class="btn btn-outline-primary" for="s">
+											S
+										</label>
+									</div>
+									<div>
+										<input type="checkbox" class="btn-check" id="m" autocomplete="off" />
+										<label class="btn btn-outline-primary" for="m">
+											M
+										</label>
+									</div>
+									<div>
+										<input type="checkbox" class="btn-check" id="l" autocomplete="off" />
+										<label class="btn btn-outline-primary" for="l">
+											L
+										</label>
+									</div>
+									<div>
+										<input type="checkbox" class="btn-check" id="xl" autocomplete="off" />
+										<label class="btn btn-outline-primary" for="xl">
+											XL
+										</label>
+									</div>
+								</div>
+							</div>
+							<div className="modal-body">
+								<h6>Category</h6>
+								<div className={`d-flex flex-wrap ${style.checkContainer} mt-3`}>
+									<div>
+										<input type="checkbox" class="btn-check" id="all" autocomplete="off" />
+										<label class="btn btn-outline-primary" for="all">
+											ALL
+										</label>
+									</div>
+									<div>
+										<input type="checkbox" class="btn-check" id="women" autocomplete="off" />
+										<label class="btn btn-outline-primary" for="women">
+											Women
+										</label>
+									</div>
+									<div>
+										<input type="checkbox" class="btn-check" id="men" autocomplete="off" />
+										<label class="btn btn-outline-primary" for="men">
+											Men
+										</label>
+									</div>
+									<div>
+										<input type="checkbox" class="btn-check" id="boys" autocomplete="off" />
+										<label class="btn btn-outline-primary" for="boys">
+											Boys
+										</label>
+									</div>
+									<div>
+										<input type="checkbox" class="btn-check" id="girls" autocomplete="off" />
+										<label class="btn btn-outline-primary" for="girls">
+											Girls
+										</label>
+									</div>
+								</div>
+							</div>
+							<div className="modal-body">
+								<h6>Brand</h6>
+								<h6>Nanti pakai collapse</h6>
+							</div>
+						</form>
 						<div className="modal-footer">
-							<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+							<button type="button" className="btn btn-outline-primary btn-modal" data-bs-dismiss="modal">
 								Close
 							</button>
-							<button type="button" className="btn btn-primary">
+							<button type="button" className="btn btn-primary btn-modal">
 								Save changes
 							</button>
 						</div>
